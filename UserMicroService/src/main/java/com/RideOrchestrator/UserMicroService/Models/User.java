a0d9b133007f6998de71c2b7222ed94b19c1434e
MidhunManu <midhunmanu384@gmail.com>
2026-05-05 00:03:20 +0530

    refactor: set roles not null

--- UserMicroService/src/main/java/com/RideOrchestrator/UserMicroService/Models/User.java
+++ UserMicroService/src/main/java/com/RideOrchestrator/UserMicroService/Models/User.java
@@ -32,6 +32,7 @@ public class User {
     @Column(name = "phone_number", length = 20, unique = true, nullable = false)
     private String phoneNumber;
 
+    @Column(nullable = false)
     private RoleEnum role;
     
     public User(Integer id, String username, String email, String phoneNumber, RoleEnum role) {
